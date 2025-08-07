b75ff593447a16f5cb7cdfd5f337a03bb10c9381
Sahil Sharma <99269975+DrVenom1@users.noreply.github.com>
2025-08-07 13:57:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+let cards = []
+let sum = 0
+let hasblackJack= false
+let isAlive= false
+let message = ""
+
+let messageEl = document.getElementById("message-el")
+let sumEl= document.getElementById("sum-el")
+let cardEl = document.getElementById("card-el")
+
+function getRandomCard(){
+    let randomNumber = Math.floor(Math.random()*13) + 1
+    if (randomNumber===1){
+        return 11
+    } else if(randomNumber>10){
+        return 10
+    }
+    else{
+        return randomNumber
+    }
+}
+
+function startGame(){
+    isAlive= true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards= [firstCard,secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame(){
+    cardEl.textContent= "Cards: "
+    for (let i = 0; i < cards.length; i++){
+        cardEl.textContent+=cards[i] + " "
+    }
+        sumEl.textContent="Sum:  "+ sum
+        if (sum<21){
+            message= "Do you want to draw new card"
+        } else if ( sum===21){
+            message = "wohoo you got blackjack"
+            hasblackJack = true
+        } else{
+            message = "You're out of game"
+            isAlive= false
+        }
+        messageEl.textContent= message
+}
+
+function newCard(){
+    if (isAlive===true && hasblackJack===false){
+        let card= getRandomCard()
+        sum += card
+        cards.push(card) 
+        renderGame()
+    }
+}
